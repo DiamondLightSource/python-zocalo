962029af3d77b3096fef91d4d0458fd69ed064e1
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-11-28 09:30:56 +0000

    Add a function to enable logging to graylog

---
+++ tests/test_graylog.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""Tests related to graylog functionality."""
+
+from __future__ import absolute_import, division, print_function
+
+import mock
+import pytest
+import zocalo
+
+
+@mock.patch("zocalo.logging")
+@mock.patch("zocalo.graypy")
+def test_that_graypy_is_correctly_initialised(graypy, logging):
+    zocalo.enable_graylog(host=mock.sentinel.host, port=mock.sentinel.port)
+    logging.getLogger.return_value.addHandler.assert_called_once_with(
+        graypy.GELFHandler.return_value
+    )
+    graypy.GELFHandler.assert_called_once()
+    assert graypy.GELFHandler.call_args[0] == (mock.sentinel.host, mock.sentinel.port)
+
+
+@mock.patch("zocalo.logging")
+@mock.patch("zocalo.graypy")
+def test_that_graypy_is_using_sensible_defaults(graypy, logging):
+    zocalo.enable_graylog()
+    graypy.GELFHandler.assert_called_once()
+    call_args = graypy.GELFHandler.call_args[0]
+    assert len(call_args) == 2
+    assert "diamond" in call_args[0]
+    assert isinstance(call_args[1], int) and call_args[1] > 0
+
+
+@mock.patch("zocalo.logging")
+@mock.patch("zocalo.graypy")
+def test_that_python_log_levels_are_translated_to_graylog_levels(graypy, logging):
+    zocalo.enable_graylog()
+    assert graypy.handler.SYSLOG_LEVELS.get(10, None) == 7
+    assert graypy.handler.SYSLOG_LEVELS.get(42, 20) == 3
+    assert graypy.handler.SYSLOG_LEVELS.get(100, "banana") == 1
--- zocalo/__init__.py
+++ zocalo/__init__.py
@@ -4,6 +4,49 @@ from __future__ import absolute_import, division, print_function
 
 """Top-level package for Zocalo."""
 
-__author__ = """Markus Gerstel"""
-__email__ = 'scientificsoftware@diamond.ac.uk'
+import graypy
+import logging
+
+__author__ = "Markus Gerstel"
+__email__ = "scientificsoftware@diamond.ac.uk"
 __version__ = '0.1.0'
+
+
+def enable_graylog(host="graylog2.diamond.ac.uk", port=12201):
+    """
+    Enable logging to a Graylog server. By default this is set up to log to
+    the default index at the Diamond Light Source central graylog instance.
+    :param host: Graylog server hostname (optional)
+    :param port: Graylog server UDP port (optional)
+    :return: graypy log handler
+    """
+
+    # Monkeypatch graypy to support graylog log levels:
+    # Translate Python integer level numbers to syslog levels
+    class PythonLevelToSyslogConverter(object):
+        @staticmethod
+        def get(level, _):
+            if level < 20:
+                return 7  # DEBUG
+            elif level < 25:
+                return 6  # INFO
+            elif level < 30:
+                return 5  # NOTICE
+            elif level < 40:
+                return 4  # WARNING
+            elif level < 50:
+                return 3  # ERROR
+            elif level < 60:
+                return 2  # CRITICAL
+            else:
+                return 1  # ALERT
+
+    graypy.handler.SYSLOG_LEVELS = PythonLevelToSyslogConverter()
+
+    # Create and enable graylog handler
+    graylog = graypy.GELFHandler(host, port, level_names=True)
+    logger = logging.getLogger()
+    logger.addHandler(graylog)
+
+    # Return the handler, which may be useful to attach filters to it.
+    return graylog
