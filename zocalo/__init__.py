e4c9ccd21b3f31797b971c83de8a36c8bca13a26
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-12-04 12:34:16 +0000

    Bump version: 0.5.0 → 0.5.1

--- zocalo/__init__.py
+++ zocalo/__init__.py
@@ -9,7 +9,7 @@ import logging
 
 __author__ = "Markus Gerstel"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 logging.getLogger("zocalo").addHandler(logging.NullHandler())
 
