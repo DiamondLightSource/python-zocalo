6fc42812298828121a29adf97eb6cae1e81540a8
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-12-04 10:20:07 +0000

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/DiamondLightSource/zocalo-python",
-    version="0.4.0",
+    version="0.5.0",
     zip_safe=False,
 )
