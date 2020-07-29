7271c0f95a4a3d94bd2b950a3775e63dc1338290
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-07-29 11:34:07 +0100

    Add pyyaml dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["graypy", "workflows"]
+requirements = ["graypy", "pyyaml", "workflows"]
 setup_requirements = []
 test_requirements = ["mock", "pytest"]
 
