825423d25a1332fc8f4f306fe14af755b241ad75
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-12-17 19:46:45 +0000

    copy dlstbx.util.symlink function
    but rewrite them using pathlib, add types and tests

---
+++ tests/util/test_symlink.py
@@ -0,0 +1,50 @@
+import zocalo.util.symlink
+
+
+def test_create_parent_symlink(tmp_path):
+    d = tmp_path / "123456-uuid-7890" / "tool"
+    d.mkdir(parents=True)
+    assert zocalo.util.symlink.create_parent_symlink(d, "some-name") is True
+    assert tmp_path.joinpath("some-name").is_symlink()
+    assert tmp_path.joinpath("some-name").resolve() == d.resolve()
+    assert (
+        zocalo.util.symlink.create_parent_symlink(
+            d, "some-name", overwrite_symlink=True
+        )
+        is True
+    )
+    assert tmp_path.joinpath("some-name").resolve() == d.resolve()
+
+    d = tmp_path / "123456-uuid-7890" / "tool-other"
+    d.mkdir()
+    assert zocalo.util.symlink.create_parent_symlink(d, "some-name") is False
+    assert tmp_path.joinpath("some-name").resolve() != d.resolve()
+    assert (
+        zocalo.util.symlink.create_parent_symlink(
+            d, "some-name", overwrite_symlink=True
+        )
+        is True
+    )
+    assert tmp_path.joinpath("some-name").resolve() == d.resolve()
+
+
+def test_create_parent_symlink_levels(tmp_path):
+    d = tmp_path / "123456-uuid-7890" / "tool"
+    d.mkdir(parents=True)
+    assert zocalo.util.symlink.create_parent_symlink(d, "some-name", levels=1) is True
+    assert (tmp_path / "123456-uuid-7890" / "some-name").is_symlink()
+    assert (tmp_path / "123456-uuid-7890" / "some-name").resolve() == d.resolve()
+
+
+def test_create_parent_symlink_does_not_overwrite_files(tmp_path):
+    d = tmp_path / "123456-uuid-7890" / "tool"
+    d.mkdir(parents=True)
+    tmp_path.joinpath("some-name").touch()
+    assert zocalo.util.symlink.create_parent_symlink(d, "some-name") is False
+    assert (
+        zocalo.util.symlink.create_parent_symlink(
+            d, "some-name", overwrite_symlink=True
+        )
+        is False
+    )
+    assert not tmp_path.joinpath("some-name").is_symlink()
---
+++ zocalo/util/symlink.py
@@ -0,0 +1,67 @@
+import pathlib
+from typing import Union
+
+
+def create_parent_symlink(
+    destination_path: Union[str, pathlib.Path],
+    symlink_name: str,
+    *,
+    levels: int = 2,
+    overwrite_symlink: bool = False,
+) -> bool:
+    """Create a symbolic link in a parent directory,
+    $levels levels above the link destination.
+    If a link already exists in that location it can be overwritten.
+    If a file with the symlink name exists in the location it is left
+    untouched.
+
+    :param destination_path: The full path that is the symlink destination.
+    :param symlink_name: The name of the symbolic link to be created.
+    :param levels: The number of levels above the destination path where the
+                   symlink should be created.
+    :param overwrite_symlink: If the destination exists and is a symlink,
+                              whether it should be overwritten.
+    :return: True if successful, False otherwise.
+    """
+
+    destination_path = pathlib.Path(destination_path)
+    assert destination_path.is_absolute()
+    assert levels > 0, "symlink must be in parent directory or above"
+
+    # Generate path to the symbolic link
+    link_path = destination_path.parents[levels - 1].joinpath(symlink_name)
+
+    # Construct the (relative) destination of the symlink
+    rel_destination = pathlib.Path(*destination_path.parts[-levels:])
+
+    return symlink_to(rel_destination, link_path, overwrite_symlink=overwrite_symlink)
+
+
+def symlink_to(
+    link_destination: pathlib.Path,
+    link_path: pathlib.Path,
+    *,
+    target_is_directory: bool = False,
+    overwrite_symlink: bool = False,
+) -> bool:
+    """Create a symbolic link.
+    This function works analogous to os.symlink, but optionally allows
+    overwriting symbolic links, and instead of raising exceptions returns
+    True on success and False on failure.
+    """
+
+    # Bail if the destination is a symbolic link and we do not overwrite links
+    if link_path.is_symlink():
+        # Python 3.9+: Could use .readlink() here and return True if correct
+        if not overwrite_symlink:
+            return False
+    elif link_path.exists():
+        # If it is not a symbolic link AND exists, then also bail.
+        return False
+
+    # Symlinks can't be directly overwritten, so create a temporary symlink next
+    # to where it should go, and then rename on top of a potentially existing link.
+    tmp_link = link_path.parent / f".tmp.{link_path.name}"
+    tmp_link.symlink_to(link_destination, target_is_directory=target_is_directory)
+    tmp_link.replace(link_path)
+    return True
