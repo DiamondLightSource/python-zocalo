cbea42096396f742e4555c659381215a606a48e6
Stu Fisher <stu@q3f.org>
2020-12-21 18:57:38 +0100

    configure graylog

--- zocalo/__init__.py
+++ zocalo/__init__.py
@@ -5,6 +5,9 @@ import logging
 import graypy
 import graypy.handler
 
+
+from zocalo.configuration import config
+
 __author__ = "Markus Gerstel"
 __email__ = "scientificsoftware@diamond.ac.uk"
 __version__ = "0.7.1"
@@ -12,7 +15,7 @@ __version__ = "0.7.1"
 logging.getLogger("zocalo").addHandler(logging.NullHandler())
 
 
-def enable_graylog(host="graylog2.diamond.ac.uk", port=12201):
+def enable_graylog():
     """
     Enable logging to a Graylog server. By default this is set up to log to
     the default index at the Diamond Light Source central graylog instance.
@@ -42,13 +45,14 @@ def enable_graylog(host="graylog2.diamond.ac.uk", port=12201):
                 return 1  # ALERT
 
     graypy.handler.SYSLOG_LEVELS = PythonLevelToSyslogConverter()
+    graylog_config = config.get_plugin("graylog", env=None)
 
     # Create and enable graylog handler
     try:
         handler = graypy.GELFUDPHandler
     except AttributeError:
         handler = graypy.GELFHandler  # graypy < 1.0
-    graylog = handler(host, port, level_names=True)
+    graylog = handler(graylog_config["host"], graylog_config["port"], level_names=True)
     logger = logging.getLogger()
     logger.addHandler(graylog)
 
