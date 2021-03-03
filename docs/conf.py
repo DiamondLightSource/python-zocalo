d7321c249125cfd4c23aa0a1585573af62a18693
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2021-03-03 14:00:55 +0000

    Build documentation with readthedocs theme (#77)

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
+import sphinx_rtd_theme  # noqa; F401 - install theme
+
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -30,7 +32,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -82,7 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
