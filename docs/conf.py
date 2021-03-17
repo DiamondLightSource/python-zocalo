a446bf660d4d173aaa9699f220965b0f39049141
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-03-17 10:23:37 +0000

    Bump version: 0.7.3 → 0.7.4

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ author = "Scientific Software"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.7.3"
+version = "0.7.4"
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -69,6 +69,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/DiamondLightSource/zocalo-python",
-    version="0.7.3",
+    version="0.7.4",
     zip_safe=False,
 )
