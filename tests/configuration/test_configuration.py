288aaa28b941df4f5b9d79bc469c516dac7703a2
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-05-10 09:56:29 +0100

    Add a few more tests

--- tests/configuration/test_configuration.py
+++ tests/configuration/test_configuration.py
@@ -50,6 +50,16 @@ def test_loading_minimal_valid_configuration():
     _assert_configuration_is_empty(zc)
 
 
+def test_cannot_load_unversioned_yaml_files():
+    with pytest.raises(RuntimeError, match="Invalid configuration"):
+        zocalo.configuration.from_string("value: 1")
+
+
+def test_cannot_load_unknown_configuration_file_versions():
+    with pytest.raises(RuntimeError, match="not understand"):
+        zocalo.configuration.from_string("version: 0")
+
+
 def test_loading_minimal_valid_configuration_from_file(tmp_path):
     config_file = tmp_path.joinpath("config.yml")
     config_file.write_text("version: 1")
@@ -57,6 +67,18 @@ def test_loading_minimal_valid_configuration_from_file(tmp_path):
     _assert_configuration_is_empty(zc)
     zc = zocalo.configuration.from_file(config_file)
     _assert_configuration_is_empty(zc)
+
+
+def test_cannot_load_missing_file(tmp_path):
+    with pytest.raises(RuntimeError, match="not found"):
+        zocalo.configuration.from_file(tmp_path / "missing.yml")
+
+
+def test_cannot_load_invalid_file(tmp_path):
+    config = tmp_path / "invalid.yml"
+    config.write_text("x: y: z:")
+    with pytest.raises(RuntimeError, match="invalid.yml"):
+        zocalo.configuration.from_file(config)
 
 
 def test_loading_sample_configuration():
@@ -79,6 +101,19 @@ def test_cannot_load_inconsistent_configuration():
         )
 
 
+def test_cannot_load_configuration_where_environments_specifies_plugin_as_string():
+    with pytest.raises(RuntimeError, match="invalid-spec"):
+        zocalo.configuration.from_string(
+            """
+            version: 1
+            environments:
+              invalid-spec: constants
+            constants:
+              plugin: storage
+            """
+        )
+
+
 def test_cannot_activate_missing_environment():
     zc = zocalo.configuration.from_string("version: 1")
     with pytest.raises(ValueError):
@@ -95,9 +130,18 @@ def test_activate_an_empty_environment():
     assert "empty" in str(zc)
 
 
-def test_activate_an_environment():
+def test_activate_one_environment():
     zc = zocalo.configuration.from_string(sample_configuration)
     zc.activate_environment("live")
     zc.active_environments.append("list should not be mutable")
     assert zc.active_environments == ["live"]
     assert "live" in str(zc)
+
+
+def test_activate_multiple_environments():
+    zc = zocalo.configuration.from_string(sample_configuration)
+    zc.activate_environment("partial")
+    zc.activate_environment("part-2")
+    assert zc.active_environments == ["partial", "part-2"]
+    assert "partial" in str(zc)
+    assert "part-2" in str(zc)
