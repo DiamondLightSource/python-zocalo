b3c0e6db088da31cbb2973200fb0100ab2e090e0
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-05-28 08:00:24 +0100

    Add a putative RabbitMQ configuration plugin
    may be subject to change

---
+++ src/zocalo/configuration/plugin_rabbitmqapi.py
@@ -0,0 +1,16 @@
+from marshmallow import fields
+
+from zocalo.configuration import PluginSchema
+
+
+class RabbitAPI:
+    class Schema(PluginSchema):
+        base_url = fields.Str(required=True)
+        username = fields.Str(required=True)
+        password = fields.Str(required=True)
+        vhost = fields.Str()
+
+    @staticmethod
+    def activate(configuration):
+        configuration.setdefault("vhost", "/")
+        return configuration
