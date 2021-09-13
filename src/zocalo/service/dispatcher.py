c171174a6ac07330c577acab088f29de2da6c7df
Stuart Fisher <stuart.fisher@esrf.fr>
2021-09-13 15:36:25 +0200

    add dispatcher and runner

---
+++ src/zocalo/service/dispatcher.py
@@ -0,0 +1,217 @@
+import copy
+import errno
+import os
+import timeit
+import uuid
+import pkg_resources
+
+from pprint import pformat
+
+import workflows.recipe
+from workflows.services.common_service import CommonService
+
+
+class Dispatcher(CommonService):
+    """
+    Single point of contact service that takes in job meta-information
+    (say, a data collection ID), a processing recipe, a list of recipes,
+    or pointers to recipes stored elsewhere, and mangles these into something
+    that can be processed by downstream services.
+    """
+
+    # Human readable service name
+    _service_name = "Dispatcher"
+
+    # Logger name
+    _logger_name = "services.dispatcher"
+
+    # Define a base path where your recipes are located (accessed with zocalo.go -r $recipename)
+    recipe_basepath = None
+
+    def filter_load_recipes_from_files(self, message, parameters):
+        """Loads named recipes from central location and merges them into the recipe object"""
+        for recipefile in message.get("recipes", []):
+            try:
+                with open(
+                    os.path.join(self.recipe_basepath, recipefile + ".json"), "r"
+                ) as rcp:
+                    named_recipe = workflows.recipe.Recipe(recipe=rcp.read())
+            except ValueError as e:
+                raise ValueError("Error reading recipe '%s': %s", recipefile, str(e))
+            except IOError as e:
+                if e.errno == errno.ENOENT:
+                    raise ValueError(
+                        f"Message references non-existing recipe {recipefile}. Recipe path is {self.recipe_basepath}",
+                    )
+                raise
+            try:
+                named_recipe.validate()
+            except workflows.Error as e:
+                raise ValueError(
+                    "Named recipe %s failed validation. %s" % (recipefile, str(e))
+                )
+            named_recipe.apply_parameters(parameters)
+            message["recipe"] = message["recipe"].merge(named_recipe)
+        return message, parameters
+
+    def filter_apply_parameters(self, message, parameters):
+        """Fill in any placeholders in the recipe of the form {name} using the
+        parameters data structure"""
+        message["recipe"].apply_parameters(parameters)
+        return message, parameters
+
+    def hook_before_filtering(self, header, message, parameters, recipe_id):
+        """Actions to be taken before message filtering
+        
+        If this function returns false then processing of this message 
+        will stop
+        """
+        return_code = True
+        for entry in pkg_resources.iter_entry_points(
+            "zocalo.dispatcher.hooks_before_filtering"
+        ):
+            fn = entry.load()
+            val = fn(header, message, parameters, recipe_id, self.transport, self.log)
+            if not val:
+                return_code = False
+
+        return return_code
+
+    def hook_before_dispatch(
+        self, header, message, parameters, recipe_id, filtered_message, rw,
+    ):
+        """Actions to be taken just before dispatching message
+        
+        If this function returns false then processing of this message
+        will stop
+        """
+        return_code = True
+        for entry in pkg_resources.iter_entry_points(
+            "zocalo.dispatcher.hooks_before_dispatch"
+        ):
+            fn = entry.load()
+            val = fn(
+                header,
+                message,
+                parameters,
+                recipe_id,
+                filtered_message,
+                rw,
+                self.transport,
+                self.log,
+            )
+            if not val:
+                return_code = False
+
+        return return_code
+
+    def initializing(self):
+        """Subscribe to the processing_recipe queue. Received messages must be acknowledged."""
+        # self._environment.get('live') can be used to distinguish live/test modes
+        self.recipe_basepath = self.config.storage["recipe_path"]
+
+        self.log.info("Dispatcher starting")
+
+        workflows.recipe.wrap_subscribe(
+            self._transport,
+            "processing_recipe",
+            self.process,
+            acknowledgement=True,
+            log_extender=self.extend_log,
+            allow_non_recipe_messages=True,
+        )
+
+    def process(self, rw, header, message):
+        """Process an incoming processing request."""
+        # Time execution
+        start_time = timeit.default_timer()
+
+        # Load processing parameters
+        parameters = message.get("parameters", {})
+        if not isinstance(parameters, dict):
+            # malformed message
+            self.log.warning(
+                "Dispatcher rejected malformed message: parameters not given as dictionary"
+            )
+            self._transport.nack(header)
+            return
+
+        # Unless 'uuid' is already defined then generate a unique recipe IDs for
+        # this request, which is attached to all downstream log records and can
+        # be used to determine unique file paths.
+        recipe_id = parameters.get("uuid") or str(uuid.uuid4())
+        parameters["uuid"] = recipe_id
+
+        if rw:
+            # If we received a recipe wrapper then we already have a recipe_ID
+            # attached to logs. Make a note of the downstream recipe ID so that
+            # we can track execution beyond recipe boundaries.
+            self.log.info(
+                "Processing request with new recipe ID %s:\n%s", recipe_id, str(message)
+            )
+
+        # From here on add the global ID to all log messages
+        with self.extend_log("recipe_ID", recipe_id):
+            self.log.debug("Received processing request:\n" + str(message))
+            self.log.debug("Received processing parameters:\n" + str(parameters))
+
+            filtered_message = copy.deepcopy(message)
+            filtered_parameters = copy.deepcopy(parameters)
+
+            # Call a hook before processing the message
+            if not self.hook_before_filtering(header, message, parameters, recipe_id):
+                return
+
+            # Create empty recipe
+            filtered_message["recipe"] = workflows.recipe.Recipe()
+
+            # Apply all specified filters in order to message and parameters
+            message_filters = [
+                f.load()
+                for f in pkg_resources.iter_entry_points("zocalo.dispatcher.filters")
+            ] + [self.filter_load_recipes_from_files, self.filter_apply_parameters]
+
+            for f in message_filters:
+                try:
+                    filtered_message, filtered_parameters = f(
+                        filtered_message, filtered_parameters
+                    )
+                except Exception as e:
+                    self.log.error(
+                        "Rejected message due to filter error: %s",
+                        str(e),
+                        exc_info=True,
+                    )
+                    self._transport.nack(header)
+                    return
+
+            self.log.debug("Mangled processing request:\n" + pformat(filtered_message))
+            self.log.debug(
+                "Mangled processing parameters:\n" + pformat(filtered_parameters)
+            )
+
+            # Create the RecipeWrapper object
+            rw = workflows.recipe.RecipeWrapper(
+                environment={"ID": recipe_id},
+                recipe=filtered_message["recipe"],
+                transport=self._transport,
+            )
+
+            # Conditionally acknowledge receipt of the message
+            txn = self._transport.transaction_begin()
+            self._transport.ack(header, transaction=txn)
+
+            # Call another hook just before dispatching the message
+            if not self.hook_before_dispatch(
+                header, message, parameters, recipe_id, filtered_message, rw,
+            ):
+                return
+
+            rw.start(transaction=txn)
+
+            # Commit transaction
+            self._transport.transaction_commit(txn)
+            self.log.info(
+                "Processed incoming message in %.4f seconds",
+                timeit.default_timer() - start_time,
+            )
---
+++ src/zocalo/service/runner.py
@@ -0,0 +1,205 @@
+import os
+import errno
+import json
+import subprocess
+from datetime import datetime
+
+import workflows.recipe
+from workflows.services.common_service import CommonService
+
+
+class Runner(CommonService):
+    """Basic Runner service to execute recipes and wrapped recipes"""
+
+    # Human readable service name
+    _service_name = "Runner"
+
+    # Logger name
+    _logger_name = "zocalo.runner"
+
+    @property
+    def custom_queue(self):
+        return os.environ.get("RUNNER_CUSTOM_QUEUE") or ""
+
+    def initializing(self):
+        self.log.info("Runner Service starting")
+
+        custom_queue = f"{self.custom_queue}." if self.custom_queue else ""
+        queue = f"runner.{custom_queue}submission"
+        self.log.info("Subscribing to: %s", queue)
+
+        workflows.recipe.wrap_subscribe(
+            self._transport,
+            queue,
+            self.process,
+            acknowledgement=True,
+            log_extender=self.extend_log,
+        )
+
+    @staticmethod
+    def _recursive_mkdir(path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                raise
+
+    def process(self, rw, header, message):
+        """Process the incoming recipes"""
+        self.log.info(
+            f"Running Runner Service at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        )
+
+        parameters = rw.recipe_step["parameters"]
+
+        try:
+            workingdir = parameters["workingdir"]
+            commands = parameters["commands"]
+        except KeyError:
+            self.log.error(
+                "Recipe did not contain both workingdir and commands", exc_info=True
+            )
+            self._transport.nack(header)
+            return
+
+        if not len(commands):
+            self.log.error("Did not receive any commands to execute")
+            self._transport.nack(header)
+            return
+
+        try:
+            self._recursive_mkdir(workingdir)
+        except OSError as e:
+            self.log.error(
+                "Could not create working directory: %s", str(e), exc_info=True
+            )
+            self._transport.nack(header)
+            return
+
+        commands = [
+            com.replace("$RECIPEPOINTER", str(rw.recipe_pointer)) for com in commands
+        ]
+
+        if "recipefile" in parameters:
+            recipefile = parameters["recipefile"]
+            try:
+                self._recursive_mkdir(os.path.dirname(recipefile))
+            except OSError as e:
+                if e.errno == errno.ENOENT:
+                    self.log.error(
+                        "Error in underlying filesystem: %s", str(e), exc_info=True
+                    )
+                    self._transport.nack(header)
+                    return
+                raise
+            self.log.debug("Writing recipe to %s", recipefile)
+            commands = [com.replace("$RECIPEFILE", recipefile) for com in commands]
+
+            with open(recipefile, "w") as fh:
+                fh.write(rw.recipe.pretty())
+
+        if "recipeenvironment" in parameters:
+            recipeenvironment = parameters["recipeenvironment"]
+            try:
+                self._recursive_mkdir(os.path.dirname(recipeenvironment))
+            except OSError as e:
+                if e.errno == errno.ENOENT:
+                    self.log.error(
+                        "Error in underlying filesystem: %s", str(e), exc_info=True
+                    )
+                    self._transport.nack(header)
+                    return
+                raise
+            self.log.debug("Writing recipe environment to %s", recipeenvironment)
+            commands = [
+                com.replace("$RECIPEENV", recipeenvironment) for com in commands
+            ]
+            with open(recipeenvironment, "w") as fh:
+                json.dump(
+                    rw.environment, fh, sort_keys=True, indent=2, separators=(",", ": ")
+                )
+
+        # Create a recipewrap file to pass to the wrapper script
+        if "recipewrapper" in parameters:
+            recipewrapper = parameters["recipewrapper"]
+            self.log.debug(f"Storing serialized recipe wrapper in {recipewrapper}")
+
+            try:
+                self._recursive_mkdir(os.path.dirname(recipewrapper))
+            except OSError as e:
+                if e.errno == errno.ENOENT:
+                    self.log.error(
+                        "Error in underlying filesystem: %s", str(e), exc_info=True
+                    )
+                else:
+                    self.log.error(
+                        "Could not create recipe wrapper directory: %s",
+                        str(e),
+                        exc_info=True,
+                    )
+                self._transport.nack(header)
+                return
+
+            try:
+                with open(recipewrapper, "w") as fh:
+                    json.dump(
+                        {
+                            "recipe": rw.recipe.recipe,
+                            "recipe-pointer": rw.recipe_pointer,
+                            "environment": rw.environment,
+                            "recipe-path": rw.recipe_path,
+                            "payload": rw.payload,
+                        },
+                        fh,
+                        indent=2,
+                    )
+            except OSError:
+                self.log.error(f"Could not write recipewrap to file at {recipewrapper}")
+                self._transport.nack(header)
+                return
+
+            # Replace the $RECIPEWRAP keyword in the command with the new recipewrapper file
+            commands = [com.replace("$RECIPEWRAP", recipewrapper) for com in commands]
+
+        # Conditionally acknowledge receipt of the message
+        txn = self._transport.transaction_begin()
+        self._transport.ack(header, transaction=txn)
+
+        try:
+            commands_list = [cmd.split(" ") for cmd in commands]
+            for cmd in commands_list:
+                result = subprocess.run(
+                    cmd,
+                    timeout=parameters.get("timeout", 30),
+                    cwd=workingdir,
+                    capture_output=parameters.get("output_file") is not None,
+                )
+                result.check_returncode()
+
+                if parameters.get("output_file"):
+                    output_file = os.path.join(workingdir, parameters["output_file"])
+                    with open(output_file, "a") as f:
+                        f.write(result.stdout.decode("utf-8"))
+
+                        if result.stderr:
+                            f.write(result.stderr.decode("utf-8"))
+
+        except (subprocess.CalledProcessError, FileNotFoundError) as e:
+            self.log.exception(f"Had error running command: {cmd} {str(e)}")
+            rw.send_to("failure", "")
+
+        except subprocess.TimeoutExpired:
+            self.log.exception(f"Time ran out running command: {cmd}")
+            rw.send_to("timeout", "")
+
+        except Exception:
+            self.log.exception(f"Could not execute commands")
+
+        # Send results onwards
+        rw.set_default_channel("job_processed")
+        rw.send({"processed": True}, transaction=txn)
+
+        # Stop processing message
+        self._transport.transaction_commit(txn)
