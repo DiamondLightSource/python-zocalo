096bf1a42df73b53c201706a513127753dd65ba7
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-04-06 09:28:37 +0100

    First implementation of a generic log config plugin

---
+++ src/zocalo/configuration/plugin_logging.py
@@ -0,0 +1,157 @@
+from __future__ import annotations
+
+import copy
+import logging
+import logging.config
+import socket
+from pprint import pprint
+from typing import Any
+
+import graypy.handler
+import logging_tree
+
+
+def _config_is_incremental(config: dict[str, Any]) -> bool:
+    forbidden_keys = {"filters", "formatters", "handlers"}
+
+    # Incremental configurations must not declare any of those
+    if any(key in config for key in forbidden_keys):
+        return False
+
+    # Incremental configurations must not attach any of those
+    # to the root logger
+    if any(key in config.get("root", {}) for key in forbidden_keys):
+        return False
+    # or any other logger
+    for logger in config.get("loggers", {}).values():
+        if any(key in logger for key in forbidden_keys):
+            return False
+
+    return True
+
+
+class LoggingIncrementer:
+    def __init__(self, setup: dict[str, Any]):
+        self._setup = setup
+        self._verbosity_level = 0
+
+    @property
+    def verbosity(self) -> int:
+        return self._verbosity_level
+
+    @verbosity.setter
+    def verbosity(self, value: int) -> None:
+        if value <= self._verbosity_level:
+            return
+        for verbosity_level in range(self._verbosity_level, value):
+            try:
+                incremental = self._setup.get("verbose", [])[verbosity_level]
+            except IndexError:
+                break
+            print(f"== Applying verbosity {verbosity_level+1} ==")
+            incremental.setdefault("version", self._setup["version"])
+            incremental.setdefault("incremental", True)
+            pprint(incremental)
+            logging.config.dictConfig(incremental)
+            self._verbosity_level = value
+            logging_tree.printout()
+
+    def __repr__(self) -> str:
+        return f"<LoggingConfiguration verbosity={self._verbosity_level}>"
+
+
+class Logging:
+    """
+    A plugin to set up consistent logging configuration
+    using a Zocalo configuration file.
+    """
+
+    @staticmethod
+    def activate(configuration, config_object):
+        logconfig = copy.deepcopy(configuration)
+
+        pprint(logconfig)
+        del logconfig["plugin"]
+        logconfig.setdefault("version", 1)
+        logconfig.setdefault("disable_existing_loggers", False)
+        logconfig.setdefault("incremental", False)
+        pprint(logconfig)
+        logging.config.dictConfig(logconfig)
+        logging_tree.printout()
+
+        if logconfig["incremental"] and not _config_is_incremental(logconfig):
+            raise ValueError(
+                "Logging configuration error: definition defines items not allowed "
+                "in an incremental definition"
+            )
+
+        for level, verbosity_def in enumerate(logconfig.get("verbose", [])):
+            if not isinstance(verbosity_def, dict):
+                raise ValueError(
+                    f"Logging configuration error: verbosity level {level+1} definition "
+                    "is not a dictionary"
+                )
+            verbosity_def.setdefault("version", logconfig["version"])
+            verbosity_def.setdefault("incremental", True)
+            if verbosity_def["incremental"] and not _config_is_incremental(
+                verbosity_def
+            ):
+                raise ValueError(
+                    f"Logging configuration error: verbosity level {level+1} definition "
+                    "defines items not allowed in an incremental definition"
+                )
+
+        return LoggingIncrementer(logconfig)
+
+
+def _monkeypatch_graypy() -> None:
+    """
+    Monkeypatch a helper class into graypy to support log levels in Graylog.
+    This translates Python integer level numbers to syslog levels.
+    """
+
+    class PythonLevelToSyslogConverter:
+        @staticmethod
+        def get(level, _):
+            if level < 20:
+                return 7  # DEBUG
+            elif level < 25:
+                return 6  # INFO
+            elif level < 30:
+                return 5  # NOTICE
+            elif level < 40:
+                return 4  # WARNING
+            elif level < 50:
+                return 3  # ERROR
+            elif level < 60:
+                return 2  # CRITICAL
+            else:
+                return 1  # ALERT
+
+    graypy.handler.SYSLOG_LEVELS = PythonLevelToSyslogConverter()
+
+
+def _resolve_hostname(host: str) -> str:
+    # Attempt to resolve the hostname only once during setup
+    # rather than once for each connection, which is the default behaviour.
+    # For a UDP handler the default would mean one lookup per logging call.
+    try:
+        return socket.gethostbyname(host)
+    except Exception:
+        return host
+
+
+def GraylogTCPHandler(*, host: str, port: int) -> logging.Handler:
+    """
+    A handler factory to enable logging to a Graylog server using graypy via TCP.
+    """
+    _monkeypatch_graypy()
+    return graypy.GELFTCPHandler(_resolve_hostname(host), port, level_names=True)
+
+
+def GraylogUDPHandler(*, host: str, port: int) -> logging.Handler:
+    """
+    A handler factory to enable logging to a Graylog server using graypy via UDP.
+    """
+    _monkeypatch_graypy()
+    return graypy.GELFUDPHandler(_resolve_hostname(host), port, level_names=True)
