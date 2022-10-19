a8e43fe26744b942bf55911b7ca99894db94d8e0
Richard Gildea <richard.gildea@diamond.ac.uk>
2022-10-19 21:21:49 +0100

    Bump version: 0.25.0 → 0.25.1

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ author = "Scientific Software"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.25.0"
+version = "0.25.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
